60204a88ffcc931f47e57372ea0424c5bfafc6cb
Danish Mehmood <danishm7012@gmail.com>
2021-08-04 18:24:10 +0500

    fix map error

--- components/Packages/Package.js
+++ components/Packages/Package.js
@@ -9,37 +9,38 @@ const Package = ({ packageData }) => {
     <div className='section'>
       <Title img='/images/banners/hero.jpg' heading='Our Packages' />
       <Container>
-        {packageData.map((p) => (
-          <Row key={p.slug} className='package-row'>
-            <Col lg={5} sm={6} xs={12}>
-              <Image
-                src={p.image.url}
-                alt={p.title}
-                height='auto'
-                width='100%'
-                className='package-image'
-              />
-            </Col>
-            <Col lg={7} sm={6} xs={12} className='pakage-detail'>
-              <h2>{p.title}</h2>
-              {p.detail && (
-                <div
-                  className='package-content container'
-                  dangerouslySetInnerHTML={{
-                    __html: md.render(p.detail),
-                  }}
-                ></div>
-              )}
-              <button className='btn btn-1' size='sm'>
-                <Link href='/GetQuote'>
-                  <a style={{ color: '#fff', textDecoration: 'none' }}>
-                    Get a Quote
-                  </a>
-                </Link>
-              </button>
-            </Col>
-          </Row>
-        ))}
+        {packageData &&
+          packageData.map((p) => (
+            <Row key={p.slug} className='package-row'>
+              <Col lg={5} sm={6} xs={12}>
+                <Image
+                  src={p.image.url}
+                  alt={p.title}
+                  height='auto'
+                  width='100%'
+                  className='package-image'
+                />
+              </Col>
+              <Col lg={7} sm={6} xs={12} className='pakage-detail'>
+                <h2>{p.title}</h2>
+                {p.detail && (
+                  <div
+                    className='package-content container'
+                    dangerouslySetInnerHTML={{
+                      __html: md.render(p.detail),
+                    }}
+                  ></div>
+                )}
+                <button className='btn btn-1' size='sm'>
+                  <Link href='/GetQuote'>
+                    <a style={{ color: '#fff', textDecoration: 'none' }}>
+                      Get a Quote
+                    </a>
+                  </Link>
+                </button>
+              </Col>
+            </Row>
+          ))}
       </Container>
     </div>
   )
